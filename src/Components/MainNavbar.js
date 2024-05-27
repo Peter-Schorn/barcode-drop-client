53e793629f795a3fdba64edbfe9a5434cc1bfa13
Peter Schorn <petervschorn@gmail.com>
2024-05-27 01:34:47 -0500

    Made it very pretty!
    Centered "Barcode Drop" bold text in nav bar along with barcode icon!

--- src/Components/MainNavbar.js
+++ src/Components/MainNavbar.js
@@ -11,19 +11,48 @@ export default class MainNavbar extends Component {
         return (
             <nav className="navbar navbar-expand-lg bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Barcode Drop</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <div className="container text-center">
+                        <a className="navbar-brand" href="#">
+                            <strong style={{
+                                fontSize: 50,
+                                fontFamily: "monospace",
+                            }}>
+                                Barcode Drop
+                                <i style={{margin: "20px"}} class="fa-solid fa-barcode"></i>
+                            </strong>
+                        </a>
+                    </div>
+                    <button 
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" 
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false" 
+                        aria-label="Toggle navigation"
+                    >
                     <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
+                    <div 
+                        className="collapse navbar-collapse" 
+                        id="navbarSupportedContent"
+                    >
+                    {/* <ul 
+                        className="navbar-nav me-auto mb-2 mb-lg-0"
+                    > */}
+                        {/* <li className="nav-item">
+                        <a 
+                            className="nav-link active" 
+                            aria-current="page" 
+                            href="#"
+                        >
+                            Home
+                        </a>
+                        </li> */}
                         {/* <li className="nav-item">
                         <a className="nav-link" href="#">Link</a>
                         </li> */}
-                    </ul>
+                    {/* </ul> */}
                     </div>
                 </div>
             </nav>
