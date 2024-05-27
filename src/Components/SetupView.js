fae6d3848bb0b989157429d504a31b4a38147a0e
Peter Schorn <petervschorn@gmail.com>
2024-05-27 03:03:40 -0500

    Added info about query/body of request in setup page

--- src/Components/SetupView.js
+++ src/Components/SetupView.js
@@ -21,6 +21,26 @@ export default class SetupView extends Component {
                         {"https://api.barcodedrop.com/scan/<user>"}
                     </a>
 
+                    <p className="pt-3">
+                        Request Body:
+                    </p>
+
+                    <p className="pt-3">
+                        Query string/form-url-encoded in the body: 
+                        <span style={{padding: "0px 10px", fontFamily: "'Courier New', monospace"}}>
+                            {"?barcode=<barcode> or ?text=<barcode>"}
+                        </span>
+                    </p>
+
+                    <p className="pb-2">
+                        In the body as JSON:
+                        <span style={{padding: "0px 10px", fontFamily: "'Courier New', monospace"}}>
+                        {`{ "barcode": "<barcode>" } or { "text": "<barcode>" }`}
+                        </span>
+                    </p>
+
+
+
                     <p className="pt-5">
                         Access your scanned barcodes at the following URL ("replace {"<user>"} with your username"):
                     </p>
